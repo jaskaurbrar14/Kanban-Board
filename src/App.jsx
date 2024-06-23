0adc1d10314d9387e134b8e523f33fbb877c202e
jaskaurbrar14 <jaskaurbrar14@gmail.com>
2024-06-23 19:44:59 -0400

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Kanban from "./Components/Kanban/Kanban";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Kanban />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
